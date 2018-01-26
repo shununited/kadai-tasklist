ebb5bb0658f9133e40c8156ec32f4a7dff5e4ada
shunmatsumura <matsumura19950621@gmail.com>
2018-01-26 09:34:39 +0000

    add fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
+  root to: 'toppages#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
   resources :tasks
+  resources :tasklists, only: [:create, :destroy]
+  
 end
